2eba86d8528c1b50d0e3af5f39338beeca0b8777
MalMWare <malkelleymartin@gmail.com>
2022-10-25 21:29:02 -0700

    coin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 // TODO: Declare any global variables we need
-
+let numberHeads = 0;
+let numberTails = 0;
+let percentHeads = 0;
+let percentTails = 0;
+let result = 0;
+let totalFlips = 0;
+let image = document.querySelector("img");
+let message = document.querySelector("#result") //getElement("result")
+let flipButton = document.querySelector("#flip")
+let headsArea = document.querySelector("#heads")
+let headsPerArea = document.querySelector("#heads-percent")
+let tailsArea = document.querySelector("#tails")
+let tailsPerArea = document.querySelector("#tails-percent")
+let clearButton = document.querySelector("#clear")
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
@@ -9,9 +22,31 @@ document.addEventListener('DOMContentLoaded', function () {
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
+    flipButton.addEventListener("click", function(e) {
+        //calculate totza number of rolls/flips
+        totalFlips ++
         // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+        let result = Math.round(Math.random() * 100)
+        // decide the range for both heads and tails 
+        //let heads 0-50
+        //let tails 51-99
+        console.log(result);
 
+        // TODO: Update image and status message in the DOM
+        if (result <= 50 ) {
+            numberHeads ++
+            console.log("numberHeads", numberHeads)
+            
+            image.src = "./assets/images/penny-heads.jpg";
+            message.textContent = "You Flipped Heads!";
+        } else if (result > 50) {
+            numberTails ++
+            console.log("numberTails", numberTails)
+            
+            image.src = "./assets/images/penny-tails.jpg";
+            message.textContent = "You Flipped Tails!";
+        }
+        
         // Update the scorboard
         // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
@@ -19,9 +54,31 @@ document.addEventListener('DOMContentLoaded', function () {
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
 
+        percentHeads = (numberHeads/totalFlips) * 100
+        percentTails = (numberTails/totalFlips) * 100
+        console.log("percentHeads", percentHeads)
+        console.log("percentTails", percentTails)
+
+        headsArea.textContent = `${numberHeads}`
+        headsPerArea.textContent = `${percentHeads}%`
+        tailsArea.textContent = `${numberTails}`
+        tailsPerArea.textContent = `${percentTails}%`
+    })
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    clearButton.addEventListener("click", function(e){
+        image.src = "./assets/images/penny-heads.jpg"
+        message.textContent = "Let's Get Rolling!"
+        numberHeads = 0
+        numberTails = 0
+        percentHeads = 0
+        percentTails = 0
+        totalFlips = 0
+        headsArea.textContent = `${numberHeads}`
+        headsPerArea.textContent = `${percentHeads}%`
+        tailsArea.textContent = `${numberTails}`
+        tailsPerArea.textContent = `${percentTails}%`
+    })
 })
